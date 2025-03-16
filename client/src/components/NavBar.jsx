08b7050939eacfcce45963d54047618860e2437a
hemant1707 <34238478+hemant1707@users.noreply.github.com>
2025-03-16 17:18:22 +0530

    Update NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react'
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom'
@@ -8,18 +7,12 @@ import { SetIsloogedInContext, IsloogedInContext } from '../App';
 export default function NavBar() {
 	const isloogedIn = useContext(IsloogedInContext);
 	const setIsloogedIn = useContext(SetIsloogedInContext);
-=======
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import {Link} from 'react-router-dom'
 
-export default function NavBar() {
->>>>>>> origin/main
 	return (
 		<>
 			<AppBar>
 				<Toolbar>
-<<<<<<< HEAD
 					<Typography variant="h5" sx={{ flexGrow: 1 }}> Login/Registration Form</Typography>
 					{!isloogedIn ?
 						<>
@@ -29,12 +22,6 @@ export default function NavBar() {
 						:
 						<Logout />
 					}
-=======
-					<Typography variant="h5" sx={{flexGrow:1}}> Login/Registration Form</Typography>
-					<Button variant="contained" to ="/login" component={Link}>Login</Button>
-					<Button variant="contained" to="/logout" component={Link}>Logout</Button>
-					<Button variant="contained" to="/signup" component={Link}>Signup</Button>
->>>>>>> origin/main
 				</Toolbar>
 			</AppBar>
 		</>
